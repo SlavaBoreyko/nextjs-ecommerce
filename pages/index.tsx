a7e9b4e005b7dbcdbacd2d052e7a6c839853a4ba
yaroslavboreyko <yaroslav.boreyko@gmail.com>
2022-06-01 20:29:00 +0200

    Next.js e-commerce app. 1) Fetching 2) Config 3) Tailwind 4) Grid & Product Page

---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from "react"
+import type { InferGetServerSidePropsType } from "next"
+import getAllProducts from "@framework/product/get-all-products"
+import { getConfig } from "@framework/api/config"
+import { Layout } from "@components/common"
+import { ProductCard } from "@components/product"
+import { Grid } from "@components/ui"
+
+export async function getStaticProps() {
+  const config = getConfig()
+  console.log(config.apiUrl)
+  const products = await getAllProducts(config)
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 4 * 60 * 60
+  }
+}
+
+export default function Home({
+  products
+}: InferGetServerSidePropsType<typeof getStaticProps>) {
+
+  return (
+    <>
+      <Grid>
+        { products.slice(0,3).map(product => 
+          <ProductCard 
+            key={product.id}
+            product={product}
+          />
+        )}
+      </Grid>
+    </>
+  )
+}
+
+Home.Layout = Layout
---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    plugins: [
+        "postcss-nesting",
+        'tailwindcss/nesting',
+        "tailwindcss",
+        "autoprefixer",
+    ]
+}
+
+// module.exports = {
+//     plugins: [
+//         require("postcss-nesting"),
+//         require("tailwindcss"),
+//         require("autoprefixer")
+//     ]
+// }
