5231fcb273701c7e5019bd6af008d025d9918d39
yaroslavboreyko <yaroslav.boreyko@gmail.com>
2022-06-03 14:17:43 +0200

    Added Navbar+Userbar

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,14 +1,15 @@
 import { FC } from 'react'
 import s from "./Layout.module.css"
-import { Footer } from "@components/common/"
+import { Footer, Navbar } from "@components/common/"
 
 const Layout: FC = ({children}) => {
     return (
         <div className={s.root}>
-        <main className='fit'>
-            { children }
-        </main>
-        <Footer />
+            <Navbar />
+            <main className='fit'>
+                { children }
+            </main>
+            <Footer />
         </div>
     )
 }
